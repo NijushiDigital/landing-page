1f80790e9345dab5986e4653b81276fb12739793
Muhammad Haikal Ali Abdillah <75706383+itshaiheree@users.noreply.github.com>
2026-06-13 23:27:56 +0700

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Menu, X, ChevronDown } from 'lucide-react'
 
-import { FaGithub, FaLinkedin, FaInstagram, FaYoutube, FaLinkedin } from 'react-icons/fa'
+import { FaGithub, FaLinkedin, FaInstagram, FaYoutube } from 'react-icons/fa'
 import { FaXTwitter } from 'react-icons/fa6'
 
 const navLinks = [
