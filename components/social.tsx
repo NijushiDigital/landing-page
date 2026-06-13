4a8fc915773a83bc95d38a75ae0f9bf0902543eb
Muhammad Haikal Ali Abdillah <75706383+itshaiheree@users.noreply.github.com>
2026-06-13 23:50:08 +0700

    Update social.tsx

--- components/social.tsx
+++ components/social.tsx
@@ -1,15 +1,6 @@
 'use client'
 
-import { FaGithub, FaLinkedin, FaInstagram, FaYoutube, FaWhatsapp } from 'react-icons/fa'
-import { FaXTwitter } from 'react-icons/fa6'
-
-const socials = [
-  { name: 'Nishia Customer Support', icon: FaWhatsapp, url: 'https://wa.me/6285163614050' },
-  { name: 'GitHub', icon: FaGithub, url: 'https://github.com/NijushiDigital' },
-  { name: 'X', icon: FaXTwitter, url: 'https://x.com/nijushidigital' },
-  { name: 'Instagram', icon: FaInstagram, url: 'https://instagram.com/nijushidigital' },
-  { name: 'LinkedIn', icon: FaLinkedin, url: 'https://www.linkedin.com/company/nijushidigital/' },
-]
+import { socials } from '@/lib/socials'
 
 export function SocialLinks() {
   return (
