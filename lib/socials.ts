36b1a74bbcd919b508d0ba2a858843377fcd7b48
Muhammad Haikal Ali Abdillah <75706383+itshaiheree@users.noreply.github.com>
2026-06-13 23:48:06 +0700

    Create socials.ts

---
+++ lib/socials.ts
@@ -0,0 +1,10 @@
+import { FaGithub, FaLinkedin, FaInstagram, FaWhatsapp } from 'react-icons/fa'
+import { FaXTwitter } from 'react-icons/fa6'
+
+export const socials = [
+  { name: 'Nishia Customer Support', icon: FaWhatsapp, url: 'https://wa.me/6285163614050' },
+  { name: 'GitHub', icon: FaGithub, url: 'https://github.com/NijushiDigital' },
+  { name: 'X', icon: FaXTwitter, url: 'https://x.com/nijushidigital' },
+  { name: 'Instagram', icon: FaInstagram, url: 'https://instagram.com/nijushidigital' },
+  { name: 'LinkedIn', icon: FaLinkedin, url: 'https://www.linkedin.com/company/nijushidigital/' },
+]
