84390bd73128d918f7b288bc8e63d02a9efa5ef8
Muhammad Haikal Ali Abdillah <75706383+itshaiheree@users.noreply.github.com>
2026-06-13 23:48:35 +0700

    Create projects.ts

---
+++ lib/projects.ts
@@ -0,0 +1,17 @@
+export const projects = [
+  {
+    logo: 'https://rukogamer.com/favicon.ico',
+    title: 'RukoGamer',
+    description: 'Complete top-up website for the Indonesian region with low prices and fast processing',
+    link: 'https://rukogamer.com',
+    github: null,
+  },
+  {
+    logo: 'https://malasngoding.dev/favicon.ico',
+    title: 'malasngoding.dev',
+    description: 'SaaS website to create unlimited custom subdomains and shortlinks!',
+    link: 'https://malasngoding.dev',
+    github: null, // kalau gak ada github, set null
+  },
+  // ... tambah project lain
+]
